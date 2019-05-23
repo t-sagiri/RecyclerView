35254bbe7a6047d45225d1622bf323a12019b34c
t-sagiri <sagirix.t@gmail.com>
2019-05-23 21:39:05 +0900

    処理内容説明コメント追記

--- app/src/main/java/com/test/testrecycleview/MainActivity.java
+++ app/src/main/java/com/test/testrecycleview/MainActivity.java
@@ -5,32 +5,57 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+/**
+ * メイン画面
+ *
+ * @ref {https://developer.android.com/guide/topics/ui/layout/recyclerview}
+ * @ref {https://github.com/googlesamples/android-RecyclerView}
+ */
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
+    // private static final String TAG = "MainActivity";
+
+    /**
+     * listに表示する要素数
+     */
     private static final int DATASET_COUNT = 60;
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+
+    /**
+     * listに表示する内容
+     */
     protected String[] mDataset;
 
+    /**
+     * 画面表示
+     *
+     * @param savedInstanceState bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 画面全体用xmlリソース
         setContentView(R.layout.activity_main);
 
-        recyclerView = (RecyclerView) findViewById(R.id.main_list_view);
+        // 画面全体用のリソースから一覧になるViewを特定し、設定を行う
+        recyclerView = findViewById(R.id.main_list_view);
         recyclerView.setHasFixedSize(true);
 
+        // ListViewと同様の機能を得るためにLinearLayoutManagerをRecyclerViewに設定
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        // アダプター
+        // listに表示する内容を生成、アダプタにセット
         initDataset();
         mAdapter = new MyAdapter(mDataset);
         recyclerView.setAdapter(mAdapter);
     }
 
+    /**
+     * DATASET_COUNTの分だけデータを生成して、mDatasetにセットする
+     */
     private void initDataset() {
         mDataset = new String[DATASET_COUNT];
         int i;
